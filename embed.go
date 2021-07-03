2493e73bf8e09c6921cece4db7cb49919f038239
Elen Eisendle <ln@calcifer.ee>
2021-07-03 15:49:55 +0200

    Some fs:embed shenanigans

--- embed.go
+++ embed.go
@@ -6,5 +6,5 @@ import "embed"
 
 // BootstrapIcons holds the bootstrap-icons svg files to render them directly into the
 // templates. it only increases the binary size by ~1MB.
-//go:embed static/icons/*
+//go:embed static/icons/*.svg
 var BootstrapIcons embed.FS
