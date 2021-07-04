4a44da6565765326707268e5bddab3d90e8c8366
Zoe Boele <zoe@astech.hu>
2021-07-04 18:23:18 +0200

    added dropdown menus

--- frontend/src/js/edea.js
+++ frontend/src/js/edea.js
@@ -1,16 +1,25 @@
  
-function highlightActiveMenuItem() {
-    const navelems = document.getElementsByClassName("nav-link");
-    for (var i = 0; i < navelems.length; i++) {
-        let currentNavE = navelems.item(i);
-        if (window.location.href.startsWith(currentNavE.href)) {
-            currentNavE.classList.add("active");
+function iterateOverHTMLCollectionAndMarkActive(uwu) {
+    for (var i = 0; i < uwu.length; i++) {
+        let OwO = uwu.item(i);
+        if (OwO.tagName.toLowerCase() != "a") {
+            continue;
+        }
+        if (window.location.href.startsWith(OwO.href)) {
+            OwO.classList.add("active");
             let srhelper = document.createElement('span');
             srhelper.innerText = "current page is: ";
             srhelper.classList.add("visually-hidden");
-            currentNavE.parentNode.insertBefore(srhelper, currentNavE);
+            OwO.parentNode.insertBefore(srhelper, OwO);
         }
     }
+}
+
+function highlightActiveMenuItem() {
+    iterateOverHTMLCollectionAndMarkActive(
+        document.getElementsByClassName("dropdown-item"));
+    iterateOverHTMLCollectionAndMarkActive(
+        document.getElementsByClassName("nav-link"));
 }
 
 highlightActiveMenuItem();
