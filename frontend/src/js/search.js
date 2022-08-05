fae33dca938b1687bfbe6192850e3be3113e1ad1
Zoe Boele <zoe@astech.hu>
2022-08-05 23:52:27 +0200

    added some style to the filter page

--- frontend/src/js/search.js
+++ frontend/src/js/search.js
@@ -54,30 +54,41 @@ if (searchParams.has('q')) {
 
 searchbox_handler();
 
+function _create_button(button_label, aria_label, color) {
+	let btn = document.createElement("button")
+	btn.classList.add("btn", "btn-outline-" + color)
+	btn.setAttribute("aria-label", aria_label)
+	btn.innerHTML = button_label
+	return btn
+}
+
 async function categories() {
 	const categories = await fetch(`/api/search_fields`).then((response) => response.json())
-	var filter_table = document.querySelector("table[id='filters'] tbody")
-	var body = document.createElement('tbody');
+	var filters_container = document.getElementById("filters-row")
 
 	const cats = Object.keys(categories)
 
 	if (cats.length > 0) {
-		let row = body.insertRow(-1)
-		row.id = "filter_row"
 
 		let i = 0;
 		cats.forEach(cat => {
-			let cell = row.insertCell(i)
-			i++;
+			let outer_div = document.createElement("div")
+			outer_div.classList.add("col-3")  // change container width here when necessary
+
+			let form_control_element_name = "filterf_" + cat
 
 			let label = document.createElement('label')
-			label.innerText = cat
+			label.innerText = cat  // TODO add human readable category name here
+			label.setAttribute("for", form_control_element_name)
+			label.classList.add("form-label")
 
-			cell.appendChild(label)
+			outer_div.appendChild(label)
 
 			let select = document.createElement('select')
-			select.name = cat
+			select.name = form_control_element_name
 			select.setAttribute("multiple", "")
+			select.setAttribute("aria-label", "filter options for " + cat)
+			select.classList.add("form-select", "form-select-sm", "mb-1")
 
 			// add an option for each value
 			categories[cat].forEach(value => {
@@ -88,13 +99,24 @@ async function categories() {
 			})
 
 			// add everything together
-			cell.appendChild(select)
+			outer_div.appendChild(select)
 
-			// TODO: add some more controls to it
+			let control_div = document.createElement("div")
+			control_div.classList.add("input-group", "input-group-sm", "mb-3")
+
+			control_div.appendChild(_create_button("&#x2264;", "select all values less than or equal to selected", "secondary"))
+			let midbtn = _create_button("&#x21bb;", "clear this filter", "primary")
+			midbtn.classList.add("form-control")
+			control_div.appendChild(midbtn)
+			control_div.appendChild(_create_button("&#x2265;", "select all values larger than or equal to selected", "secondary"))
+
+			outer_div.appendChild(control_div)
+
+			filters_container.appendChild(outer_div)
+			i++
 		})
 	}
 
-	filter_table.parentNode.replaceChild(body, filter_table)
 }
 
 categories();
@@ -115,10 +137,10 @@ async function do_search() {
 		var op_values = []
 
 		for (var j = 0; j < collection.length; j++) {
-			op_values.push(collection[j].value)
+			op_values.add(collection[j].value)
 		}
 
-		filter_ops.push({ 'field': e.name, 'op': '=', 'values': op_values })
+		filter_ops.add({ 'field': e.name, 'op': '=', 'values': op_values })
 	}
 
 	const results = await fetch(
